4bc43264d1d5d21616bba66083f8913bc036fdb6
Awa TIAM [SNT DSI/DAC/DIF/SDD] <awa.tiam@orange-sonatel.com>
2025-04-04 08:15:34 +0000

    Successfully created and ran dockerized image of prediction test and predictio application via FastAPI

--- src/models/serve/up.sh
+++ src/models/serve/up.sh
@@ -1,13 +1,13 @@
 # Run at projet root level (important !)
 
 # build the image
-docker build -t predict_app_test -f src/models/predict/Dockerfile .
+docker build -t predict_app_serve -f src/models/serve/Dockerfile .
 
 # create the container : replace the `absolute` path to models/ and data/ directories with your own
 
-docker run --name predict_container_test \
+docker run --name api_serve_version_1 \
     --env-file .env \
     -v /Users/tiam028713/Documents/Formations/B2B_DataScientest_2025/Projet_Formation_MLOps_DataScientest_2025/models:/app/models \
-    -v /Users/tiam028713/Documents/Formations/B2B_DataScientest_2025/Projet_Formation_MLOps_DataScientest_2025/data:/app/data \
-    -p 8000:8000 \
-    predict_app_test
+    -v /Users/tiam028713/Documents/Formations/B2B_DataScientest_2025/Projet_Formation_MLOps_DataScientest_2025/logs:/app/logs \
+    -p 8001:8001 \
+    predict_app_serve:latest
