79a5c200b36eba2d2419a820924d5ba39bf28033
Awa TIAM [SNT DSI/DAC/DIF/SDD] <awa.tiam@orange-sonatel.com>
2025-04-04 07:06:35 +0000

    Successfully tested the dockerized prediction test on train data

--- src/models/predict/up.sh
+++ src/models/predict/up.sh
@@ -3,11 +3,11 @@
 # build the image
 docker build -t predict_app -f src/models/predict/Dockerfile .
 
-# create the container : to the .env file, add the following
+# create the container : to the .env file
 
 docker run --name predict_container \
     --env-file .env \
-    -v models/:/app/models \
-    -v data/:/app/data \
+    -v /Users/tiam028713/Documents/Formations/B2B_DataScientest_2025/Projet_Formation_MLOps_DataScientest_2025/models:/app/models \
+    -v /Users/tiam028713/Documents/Formations/B2B_DataScientest_2025/Projet_Formation_MLOps_DataScientest_2025/data:/app/data \
     -p 8000:8000 \
     predict_app
